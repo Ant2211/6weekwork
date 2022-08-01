52ba50aa6e9250d0bfd0ebbc0aa5955899be17b6
Ant2211 <turpak85@gmail.com>
2022-08-01 17:15:28 +0300

    delete search Kyiv

--- index.js
+++ index.js
@@ -71,5 +71,5 @@ function cityWeather(response) {
   let currentYear = now.getFullYear();
   let h4 = document.querySelector("h4");
   h4.innerHTML = `${currentDate} ${currentMonth} ${currentYear}, ${currentDay}`;
-  searchCity("Kyiv");
+  
   
