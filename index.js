fd3288a4923e2b66ae8c6955295ff095235b5d55
Ant2211 <turpak85@gmail.com>
2022-08-03 17:58:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,13 @@ function cityWeather(response) {
   let currentMonth = months[now.getMonth()];
   let currentYear = now.getFullYear();
   let currentHour = now.getHours();
+  if (currentHour < 10){
+    currentHour = `0 ${currentHour}`;
+  }
   let currentMinutes = now.getMinutes();
+  if (currentMinutes < 10){
+    currentMinutes = `0 ${currentMinutes}`;
+  }
   let currentShowDate = document.querySelector("#currentDate");
   currentShowDate.innerHTML = `${currentDay}, ${currentDatePosition} ${currentMonth} ${currentYear}`;
    let currentShowTime = document.querySelector ("#currentTime");
