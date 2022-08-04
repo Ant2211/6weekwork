e1fdfd141f40d616281ce869ae0f54ab037cc1fc
Ant2211 <turpak85@gmail.com>
2022-08-04 22:19:10 +0300

    add celsius/fahrenheit

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function cityWeather(response) {
     let h1 = document.querySelector("h1");
-    h1.innerHTML = response.data.name;
     let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = `${Math.round(response.data.main.temp)}`;
     let humidityElement = document.querySelector("#humidity");
-    humidityElement.innerHTML = `Humadity: ${response.data.main.humidity}%`;
     let windElement = document.querySelector("#wind");
-    windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
     let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML = response.data.weather[0].main;
     let iconElement = document.querySelector ("#icon");
-    iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  }
+    
+  celsiusTemperature = response.data.main.temp;
+
+  h1.innerHTML = response.data.name;
+  temperatureElement.innerHTML = `${Math.round(celsiusTemperature)}`;
+humidityElement.innerHTML = `Humadity: ${response.data.main.humidity}%`;
+windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)} km/h`;
+descriptionElement.innerHTML = response.data.weather[0].main;
+iconElement.setAttribute ("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+}
   
   function searchCity(city) {
     let apiKey = "e0011d9afadcdf29795388bf3f4d5677";
@@ -23,19 +26,46 @@ function cityWeather(response) {
     let city = document.querySelector("#input-city").value;
     searchCity(city);
   }
-  
-    let searchForm = document.querySelector("#search");
+  function showFahrenheitTemp (event) {
+        event.preventDefault ();
+        celsiusLink.classList.remove ("active");
+        fahrenheitLink.classList.add ("active");
+        let fahrenheitTemp = (celsiusTemperature * 9)/5 + 32;
+        let temperatureElement = document.querySelector ("#temperature");
+        temperatureElement.innerHTML = Math.round(fahrenheitTemp);
+    
+    }
+    function showCelsiusTemp (event) {
+        event.preventDefault ();
+        fahrenheitLink.classList.remove ("active");
+        celsiusLink.classList.add ("active");
+        let temperatureElement = document.querySelector ("#temperature");
+        temperatureElement.innerHTML = Math.round (celsiusTemperature);
+   }
+        let searchForm = document.querySelector("#search");
   searchForm.addEventListener("click", buttonSubmit);
-      searchCity(city);
+    
+   let celsiusTemperature = null;
+    let fahrenheitLink = document.querySelector ("#fahrenheit-link");
+       fahrenheitLink.addEventListener ("click", showFahrenheitTemp);
+       let celsiusLink = document.querySelector ("#celsius-link");
+       celsiusLink.addEventListener ("click", showCelsiusTemp);
+searchCity(city);
+        
 
-      function searchLocation(position) {
-        let apiKey = "e0011d9afadcdf29795388bf3f4d5677";
-        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+function searchLocation(position) {
+    let apiKey = "e0011d9afadcdf29795388bf3f4d5677";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  
+    axios.get(apiUrl).then(cityWeather);
+  }
+  navigator.geolocation.getCurrentPosition(searchLocation);
       
-        axios.get(apiUrl).then(cityWeather);
-      }
-      
-        navigator.geolocation.getCurrentPosition(searchLocation);
+       
+
+
+
+  
   
   let now = new Date();
   let days = [
@@ -77,6 +107,7 @@ function cityWeather(response) {
   currentShowDate.innerHTML = `${currentDay}, ${currentDatePosition} ${currentMonth} ${currentYear}`;
    let currentShowTime = document.querySelector ("#currentTime");
    currentShowTime.innerHTML = `Last update: ${currentHour}:${currentMinutes}`;
+
 
   
   
